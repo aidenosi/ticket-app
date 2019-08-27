36716efd394eccc1d61ba8a034a3e168c8205f44
aidenosi <aidenosi@yahoo.ca>
2019-08-27 11:11:33 -0400

    Removed build from .gitignore.

---
+++ build/precache-manifest.f7b8ec9224e5c3e66d6b3e7b60d96bf3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44bf668959736617880e43464b86674a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9f08a20f2f306563775d",
+    "url": "/static/css/main.dc00a612.chunk.css"
+  },
+  {
+    "revision": "3d2d8c587e4445670d82",
+    "url": "/static/js/2.cb504c28.chunk.js"
+  },
+  {
+    "revision": "9f08a20f2f306563775d",
+    "url": "/static/js/main.7bf7924f.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
